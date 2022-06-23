c66c8cdfe1cfaa861c5b60301970e385c1530d57
shahrizoda <shahrizodadeveloping@gmail.com>
2022-06-23 21:45:34 +0500

    initial

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 const openDrodpwn = document.querySelector('.open-dropdown');
 const cabinetdropdownContent = document.querySelector('.cabinet__dropdown-content');
+const dropdownClose = document.querySelector('.dropdownClose');
 
 openDrodpwn.addEventListener('click' , (e)=>{
     cabinetdropdownContent.classList.toggle('active')
+    dropdownClose.classList.toggle('active')
+    dropdownClose.addEventListener('click', (e)=>{
+        cabinetdropdownContent.classList.remove('active')
+        dropdownClose.classList.remove('active')
+    })
 })
 
 const dropdownBtn = document.querySelectorAll('.dropdown__button');
@@ -13,6 +19,13 @@ dropdownBtn.forEach(el => {
             btn.classList.remove('active')
         })
         el.classList.toggle('active')
+        dropdownClose.classList.toggle('active')
+        dropdownClose.addEventListener('click', (e)=>{
+            dropdownBtn.forEach(btn => {
+                btn.classList.remove('active')
+                dropdownClose.classList.remove('active')
+            })
+        })
     })
 });
 
@@ -22,3 +35,21 @@ const cabinetFixed = document.querySelector('.cabinet__fixed');
 fixedClose.addEventListener('click', (e)=>{
     cabinetFixed.classList.toggle('hidden')
 })
+
+const headerDropdownBtn = document.querySelectorAll('.header__dropdown-btn');
+const headerDropdownContent = document.querySelectorAll('.header__dropdown-content');
+
+headerDropdownBtn.forEach(el => {
+    el.addEventListener('click' , (e)=>{
+        headerDropdownBtn.forEach(btn => {
+            btn.classList.remove('active')
+        })
+        el.classList.toggle('active')
+        dropdownClose.classList.toggle('active')
+        dropdownClose.addEventListener('click', (e)=>{
+            headerDropdownBtn.forEach(btn => {
+                btn.classList.remove('active')
+            })
+        })
+    })
+});
