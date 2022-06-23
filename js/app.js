10d47acb7a3c6093ecf7456684a0db4080239e95
shahrizoda <shahrizodadeveloping@gmail.com>
2022-06-23 20:37:21 +0500

    initial

--- js/app.js
+++ js/app.js
@@ -9,6 +9,16 @@ const dropdownBtn = document.querySelectorAll('.dropdown__button');
 
 dropdownBtn.forEach(el => {
     el.addEventListener('click' , (e)=>{
+        dropdownBtn.forEach(btn => {
+            btn.classList.remove('active')
+        })
         el.classList.toggle('active')
     })
 });
+
+const fixedClose = document.querySelector('.cabinet__fixed-close');
+const cabinetFixed = document.querySelector('.cabinet__fixed');
+
+fixedClose.addEventListener('click', (e)=>{
+    cabinetFixed.classList.toggle('hidden')
+})
